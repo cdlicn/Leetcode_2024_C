1e0865653df3da63fa35825584d17e77d0f0d8c1
Liyuanzhe2001 <619356147@qq.com>
2024-05-05 20:47:50 +0800

    2024-05-04

---
+++ main_1235.cpp
@@ -0,0 +1,26 @@
+#include "array"
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+// TODO 1235. 规划兼职工作
+class Solution {
+public:
+    int jobScheduling(vector<int> &startTime, vector<int> &endTime, vector<int> &profit) {
+        int n = startTime.size();
+        vector<array<int, 3>> jobs(n);
+        for (int i = 0; i < n; i++) {
+            jobs[i] = {endTime[i], startTime[i], profit[i]};
+        }
+        sort(jobs.begin(), jobs.end(), [](auto &a, auto &b) { return a[0] < b[0]; }); // 按照结束时间排序
+
+        vector<int> f(n + 1);
+        for (int i = 0; i < n; i++) {
+            int j = upper_bound(jobs.begin(), jobs.begin() + i, array<int, 3>{jobs[i][1], INT_MAX}) - jobs.begin();
+            // 状态转移中，为什么是 j 不是 j+1：上面算的是 > 开始时间，-1 后得到 <= 开始时间，但由于还要 +1，抵消了
+            f[i + 1] = max(f[i], f[j] + jobs[i][2]);
+        }
+        return f[n];
+    }
+};
