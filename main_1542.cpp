72b056c2219791bade433df17967346c9810b5ea
Liyuanzhe2001 <619356147@qq.com>
2024-05-20 22:11:26 +0800

    2024-05-20

---
+++ main_1542.cpp
@@ -0,0 +1,26 @@
+#include "string"
+#include "unordered_map"
+using namespace std;
+
+// TODO 1542. 找出最长的超赞子字符串
+int longestAwesome(string s) {
+    int n = s.size();
+    unordered_map<int, int> prefix = {{0, -1}};
+    int ans = 0;
+    int sequence = 0;
+    for (int j = 0; j < n; ++j) {
+        int digit = s[j] - '0';
+        sequence ^= (1 << digit);
+        if (prefix.count(sequence)) {
+            ans = max(ans, j - prefix[sequence]);
+        } else {
+            prefix[sequence] = j;
+        }
+        for (int k = 0; k < 10; ++k) {
+            if (prefix.count(sequence ^ (1 << k))) {
+                ans = max(ans, j - prefix[sequence ^ (1 << k)]);
+            }
+        }
+    }
+    return ans;
+}
