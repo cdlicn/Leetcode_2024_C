c59f79d3fd0089db73ac4ffcefe28a7f1f4c64b8
Liyuanzhe2001 <619356147@qq.com>
2024-07-06 22:13:48 +0800

    2024-07-06

--- main_3101.cpp
+++ main_3101.cpp
@@ -5,21 +5,14 @@ using namespace std;
 long long countAlternatingSubarrays(vector<int> &nums) {
     long long res = 0;
     int n = nums.size();
-    int pre = 0;
-    long long l = 0;
-    for (int i = 1; i < n; ++i) {
-        if (nums[i] == nums[i - 1]) {
-            l = i - pre;
-            res += l * (l + 1) >> 1;
-            pre = i;
-            cout << res << endl;
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        if (i > 0 && nums[i] != nums[i - 1]) {
+            cnt++;
+        } else {
+            cnt = 1;
         }
-    }
-    if (pre == nums.size() - 1) {
-        res++;
-    } else {
-        l = n - pre;
-        res += l * (l + 1) >> 1;
+        res += cnt;
     }
     return res;
 }
