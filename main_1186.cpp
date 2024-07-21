c59625e44f48645767a2cad4110be58a3711d5e7
Liyuanzhe2001 <619356147@qq.com>
2024-07-21 20:42:02 +0800

    2024-07-21

---
+++ main_1186.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int maximumSum(vector<int> &arr) {
+    int dp0 = arr[0], dp1 = 0, res = arr[0];
+    for (int i = 1; i < arr.size(); ++i) {
+        dp1 = max(dp0, dp1 + arr[i]);
+        dp0 = max(dp0, 0) + arr[i];
+        res = max({res, dp0, dp1});
+    }
+    return res;
+}
