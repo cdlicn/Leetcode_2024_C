4a5fdf21239f43dca0466893bdd8e052dbac8e5d
Liyuanzhe2001 <619356147@qq.com>
2024-08-12 19:41:30 +0800

    2024-08-12

---
+++ main_676.cpp
@@ -0,0 +1,39 @@
+#include <utility>
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class MagicDictionary {
+public:
+    MagicDictionary() {}
+
+    void buildDict(vector<string> dictionary) {
+        words = std::move(dictionary);
+    }
+
+    bool search(string searchWord) {
+        for (auto &&word: words) {
+            if (word.size() != searchWord.size()) {
+                continue;
+            }
+
+            int diff = 0;
+            for (int i = 0; i < word.size(); ++i) {
+                if (word[i] != searchWord[i]) {
+                    ++diff;
+                    if (diff > 1) {
+                        break;
+                    }
+                }
+            }
+            if (diff == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    vector<string> words;
+};
