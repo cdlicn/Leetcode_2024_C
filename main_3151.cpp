5c1cace2f6d31624b4b52fa542dd9c4182da4676
Liyuanzhe2001 <619356147@qq.com>
2024-08-13 19:25:56 +0800

    2024-08-13

---
+++ main_3151.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+bool isArraySpecial(vector<int> &nums) {
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] % 2 == nums[i - 1] % 2) {
+            return false;
+        }
+    }
+    return true;
+}
