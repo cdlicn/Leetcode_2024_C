d012a3f38830477ada84528a367dfcd228b385c3
Liyuanzhe2001 <619356147@qq.com>
2024-08-20 19:42:34 +0800

    2024-08-20

---
+++ main_3154.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+// TODO 3154. 到达第 K 级台阶的方案数
+int waysToReachStair(int k) {
+    unordered_map<long long, int> memo;
+    auto dfs = [&](auto &&dfs, int i, int j, bool pre_down) -> int {
+        if (i > k + 1) { // 无法到达终点 k
+            return 0;
+        }
+        // 把状态 (i, j, pre_down) 压缩成一个 long long
+        long long mask = (long long) i << 32 | j << 1 | pre_down;
+        if (memo.contains(mask)) { // 之前算过了
+            return memo[mask];
+        }
+        int res = i == k;
+        res += dfs(dfs, i + (1 << j), j + 1, false); // 操作二
+        if (i && !pre_down) {
+            res += dfs(dfs, i - 1, j, true); // 操作一
+        }
+        return memo[mask] = res; // 记忆化
+    };
+    return dfs(dfs, 1, 0, false);
+}
