66438e60b3ac1495ec7cbb2a3d83556eaa28cd7b
Liyuanzhe2001 <619356147@qq.com>
2024-09-07 19:31:42 +0800

    2024-09-07

---
+++ main_3177.cpp
@@ -0,0 +1,18 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+// TODO 3177. 求出最长好子序列 II
+int maximumLength(vector<int> &nums, int k) {
+    unordered_map<int, vector<int>> fs;
+    vector<int> mx(k + 2);
+    for (int x: nums) {
+        auto &f = fs[x];
+        f.resize(k + 1);
+        for (int j = k; j >= 0; j--) {
+            f[j] = max(f[j], mx[j]) + 1;
+            mx[j + 1] = max(mx[j + 1], f[j]);
+        }
+    }
+    return mx[k + 1];
+}
