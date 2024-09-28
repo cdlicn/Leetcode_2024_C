2f9ca3fc47d8a97c1aea309e3f4fbda8a4e0bdbe
Liyuanzhe2001 <619356147@qq.com>
2024-09-28 17:19:28 +0800

    2024-09-28

--- main_2286.cpp
+++ main_2286.cpp
@@ -90,8 +90,3 @@ public:
         return true;
     }
 };
-
-作者：灵茶山艾府
-        链接：https://leetcode.cn/problems/booking-concert-tickets-in-groups/solutions/1523876/by-endlesscheng-okcu/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
