341de9d1ce7ec2bfcab46c2c9a116697957879a6
Liyuanzhe2001 <619356147@qq.com>
2024-10-09 19:30:59 +0800

    2024-10-09

---
+++ main_3171.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int minimumDifference(vector<int>& nums, int k) {
+    int ans = INT_MAX;
+    for (int i = 0; i < nums.size(); i++) {
+        int x = nums[i];
+        ans = min(ans, abs(x - k));
+        // 如果 x 是 nums[j] 的子集，就退出循环
+        for (int j = i - 1; j >= 0 && (nums[j] | x) != nums[j]; j--) {
+            nums[j] |= x;
+            ans = min(ans, abs(nums[j] - k));
+        }
+    }
+    return ans;
+}
