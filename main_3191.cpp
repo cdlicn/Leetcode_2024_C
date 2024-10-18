dc24d5e536f6bb989347c6035a502b0e8c7bc95e
Liyuanzhe2001 <619356147@qq.com>
2024-10-18 21:18:10 +0800

    2024-10-18

---
+++ main_3191.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int minOperations(vector<int> &nums) {
+    int n = nums.size();
+    int res = 0;
+    for (int i = 0; i < n - 2; i++) {
+        if (nums[i] == 0) {
+            nums[i + 1] ^= 1;
+            nums[i + 2] ^= 1;
+            res++;
+        }
+    }
+    return nums[n - 2] && nums[n - 1] ? res : -1;
+}
