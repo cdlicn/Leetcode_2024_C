6efb2f5c8656ad1b39d13176bf7c0b0c5381d5b3
Liyuanzhe2001 <619356147@qq.com>
2024-11-09 22:00:09 +0800

    2024-11-09

---
+++ main_3242.cpp
@@ -0,0 +1,68 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class NeighborSum {
+private:
+    vector<vector<int>> grid;
+    int n = 0;
+    int m = 0;
+public:
+    NeighborSum(vector<vector<int>> &grid) {
+        this->grid = grid;
+        this->n = grid.size();
+        this->m = grid[0].size();
+    }
+
+    int adjacentSum(int value) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == value) {
+                    int sum = 0;
+                    if (i - 1 >= 0) {
+                        sum += grid[i - 1][j];
+                    }
+                    if (i + 1 < n) {
+                        sum += grid[i + 1][j];
+                    }
+                    if (j - 1 >= 0) {
+                        sum += grid[i][j - 1];
+                    }
+                    if (j + 1 < m) {
+                        sum += grid[i][j + 1];
+                    }
+                    return sum;
+                }
+            }
+        }
+        return 0;
+    }
+
+    int diagonalSum(int value) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == value) {
+                    int sum = 0;
+                    if (i - 1 >= 0) {
+                        if (j - 1 >= 0) {
+                            sum += grid[i - 1][j - 1];
+                        }
+                        if (j + 1 < m) {
+                            sum += grid[i - 1][j + 1];
+                        }
+                    }
+                    if (i + 1 < n) {
+                        if (j - 1 >= 0) {
+                            sum += grid[i + 1][j - 1];
+                        }
+                        if (j + 1 < m) {
+                            sum += grid[i + 1][j + 1];
+                        }
+                    }
+                    return sum;
+                }
+            }
+        }
+        return 0;
+    }
+};
